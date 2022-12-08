ece5158ddf90d8a5b57cabaa97c23331ab11ef86
Damien <damiendms450@gmail.com>
2022-12-08 15:29:20 +0100

    connrxion

--- config/appConfig.php
+++ config/appConfig.php
@@ -1,1 +1,21 @@
 <?php
+
+if(!defined('DUMP')) {
+    define('DUMP', true);
+}
+
+
+define('URL_BASE', "http://localhost/projet_bts_php");
+
+
+$infoBdd = array(
+    'interface' => 'pdo',
+    'type'   => 'mysql',
+    'host'   => 'localhost',
+    'port'   =>  3306,
+    'charset' => 'UTF8',
+    'dbname' => 'projet_bts_php',
+    'user'   => 'root',
+    'pass'   => '',
+);
+require_once 'globalConfig.php';
